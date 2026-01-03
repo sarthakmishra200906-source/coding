4ed59a42f79ab02e9fc68786a1afcb6818475cb1
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-01-03 21:54:22 +0530

    hellowworld in java

--- java/hellowworld.java
+++ java/hellowworld.java
@@ -1,4 +1,4 @@
-public class hellowworld {   // Change 'Main' to 'hellowworld'
+public class hellowworld {  
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
