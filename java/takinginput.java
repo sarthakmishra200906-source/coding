ad3433c542d11c0b0ffb994e0e3bde6861061310
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-01-06 22:49:53 +0530

    taking input and output

--- java/takinginput.java
+++ java/takinginput.java
@@ -3,7 +3,7 @@ public class takinginput{
     public static void main(String[] args) {
         Scanner sc  =new Scanner(System.in);
         System.out.println("Enter your name");
-        String name = sc.next();
+        String name = sc.nextLine();
         System.out.println("name is : "+ name);
         System.out.println("Enter your number ");
         int num_1 = sc.nextInt();
