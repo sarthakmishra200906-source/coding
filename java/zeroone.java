a02c892068231843e2d0dc84b517dcaa665b9ae3
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-01-18 18:16:45 +0530

    printed zero and one triangle logic if i +j whole modlus 2 == zero print 1 else print 0

---
+++ java/zeroone.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class zeroone {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length of zero one pryamid : ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
