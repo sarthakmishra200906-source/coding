f8b33c2586a8624828a950761a715103fcc78f95
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-01-22 13:39:06 +0530

    pushing code

---
+++ java/butterfly.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class butterfly {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        for(int i =1 ;i<=a;i++){
+
+        for(int j= 2; j<=2;j++){
+            System.out.print(" ");
+        }
+        
+
+    }
+}
---
+++ java/profun.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class profun {
+    public static int sum(int a,int b){
+     int sum =a*b;
+     return sum;
+    }
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter your first number :");
+      
+      int a= sc.nextInt();
+       System.out.println("Enter your first number :");
+      int b= sc.nextInt();
+      sum(a,b);
+      int sum=sum(a,b);
+      System.out.println("your product of 2 no is :"+ sum);
+
+
+    }
+}
