ec1c923873b3965fcea0a724ed3d15e92b5170b5
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-01-23 22:19:11 +0530

    avrage of 3 no using funstion

--- java/butterfly.java
+++ java/butterfly.java
@@ -3,11 +3,8 @@ public class butterfly {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        for(int i =1 ;i<=a;i++){
-
-        for(int j= 2; j<=2;j++){
-            System.out.print(" ");
-        }
+        
+       
         
 
     }
