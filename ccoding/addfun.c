eb03f15a5f9039586a60de2fa89aba1a61badcc0
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-01-23 21:53:35 +0530

    added exe file also

---
+++ ccoding/addfun.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int add(int a, int b){
+
+  
+}
+int main (){
+    int a,b;
+    printf("\nEnter 1st number: ");
+    scanf("%d",&a);
+    printf("\nEnter 2st number: ");
+    scanf("%d",&b);
+    int c;
+    add(a,b);
+  
+    printf(" your addition is %d",c);
+}
