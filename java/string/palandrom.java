b3d84531998a27ce7477b5ec243937a09ba57db2
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-02-08 17:37:27 +0530

    learned to check weather the string is a palandrom or not

---
+++ java/string/palandrom.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class palandrom {
+    public static boolean isPalandrome(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            int n = str.length();
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                // not a palandrom
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "sarthak";
+        System.out.println(isPalandrome(str));
+
+    }
+
+}
