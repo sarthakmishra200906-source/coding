b360542ea13425c899019ca2815f82f36f17f51e
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-02-10 08:00:08 +0530

    learned the concept of string builder

---
+++ java/string/strbuild.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class strbuild {
+   public static void main(String[] args) {
+    StringBuilder sb= new StringBuilder("");
+    for(char ch ='a';ch<='z';ch++){
+        sb.append(ch);
+        sb.append(" ");
+    }
+    System.out.println( sb);
+   } 
+}
