1321dd43881028d42c5bec58d4f612ac37968889
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-02-10 08:45:45 +0530

    learned t use string biuilder and fic=xed warning

--- java/string/strbuild.java
+++ java/string/strbuild.java
@@ -1,11 +1,12 @@
-import java.util.*;
+
 public class strbuild {
-   public static void main(String[] args) {
-    StringBuilder sb= new StringBuilder("");
-    for(char ch ='a';ch<='z';ch++){
-        sb.append(ch);
-        sb.append(" ");
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("");
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            sb.append(ch);
+            sb.append(" ");
+        }
+        System.out.println(sb);
     }
-    System.out.println( sb);
-   } 
+
 }
