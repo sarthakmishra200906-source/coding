1cc7e0e852f83cd67dd6557e32717649ae8834ba
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-02-13 06:49:39 +0530

    learend about object and classes basic defining in java

--- java/bitmanupulation/clearbit.java
+++ java/bitmanupulation/clearbit.java
@@ -6,6 +6,21 @@ public class clearbit {
         return n&bitMask;
         
     }
+     public static int setIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        return n | bitMask;
+    }
+
+    public static int updateIthBit(int n,int i,int newBit ){
+        if(newBit==0){
+            return clearIthBit(n,i);
+
+        }else{
+            return setIthBit(n,i);
+            
+        }
+        
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
