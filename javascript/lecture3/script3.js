95662f3046746aad3b9f4382b8d5993dc55fb8e6
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-03-12 00:00:38 +0530

    finished lecture 3

--- javascript/lecture3/script3.js
+++ javascript/lecture3/script3.js
@@ -1,5 +1,5 @@
 // for of loop 
-let str = "sarthakmishra";
+let str = " sarthak mishra";
 let size = 0;
 for (let i of str) {
     console.log("i = ", i);
@@ -29,3 +29,18 @@ let obj = {
 let output = `the cost of ${obj.item} is ${obj.price} rupees`;
 console.log(output);
 console.log("the cost of", obj.item, "is", obj.price, "rupees");
+//string method
+let newStr = str.toUpperCase();
+console.log(str);
+console.log(newStr);
+let lwstr = str.toLowerCase();
+console.log(lwstr);
+let nwstr=str.trim();
+console.log(nwstr);
+// method
+//str.slice(start,end?) retur part of string
+//str1.cocat(str2)joins two string
+//str.replace(searchVal,newVal)
+//str.charAt(idx)
+// promt can be used to take input from user
+
