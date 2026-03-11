90c8787bd03354618d82ddc4fba759318af6aa24
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-03-11 23:31:37 +0530

    learned type of loop in js

--- javascript/lecture3/script3.js
+++ javascript/lecture3/script3.js
@@ -1,4 +1,18 @@
-let str = "sarthakmishra";
-for (let i of str) {
-    console.log("i = ", i);
+// for of loop 
+ let str = "sarthakmishra";
+ let size=0;
+ for (let i of str) {
+     console.log("i = ", i);
+     size++;
+ }
+ console.log(size);
+//for in loop
+let student ={
+name :"sarthak",
+age: 20,
+cgpa:8.2,
+ispass:true
+};
+for(let key in student){
+    console.log("key=",key,"value=",student[key]);
 }
