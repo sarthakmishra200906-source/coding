8171550e0fd949cbea841a86166f29db7b15e30a
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-03-12 19:11:01 +0530

    learning about event

--- javascript/lecture6/script.js
+++ javascript/lecture6/script.js
@@ -1,6 +1,27 @@
 let div = document.querySelector("div");
-console.log(div);
-let id = div.getAttribute("id");
-console.log(id);
-let name = div.getAttribute("name");
-console.log(name);
+let btn1 = document.querySelector("#btn1");
+
+// 1. Get Attributes
+console.log(div.getAttribute("id"));
+console.log(div.getAttribute("name"));
+
+// 2. Button Click Event
+btn1.onclick = (evt) => {
+    console.log("Button 1 was clicked");
+    let a = 25;
+    a++;
+    console.log(a); // 26
+    
+    // Fixed capitalization for X and Y
+    console.log(evt.type);
+    console.log(evt.target);
+    console.log(evt.clientX, evt.clientY); 
+};
+
+// 3. Mouseover Event (Removed the extra "let")
+div.onmouseover = (evt) => {
+    console.log("You are inside div");
+    console.log(evt.type);
+    console.log(evt.target);
+    console.log(evt.clientX, evt.clientY);
+};
