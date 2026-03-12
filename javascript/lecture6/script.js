f38911120bef40798a845debfabc68c0dfd35d3a
sarthakmishra200906-source <sarthakmishra200906@gmail.com>
2026-03-12 22:19:38 +0530

    saving code

--- javascript/lecture6/script.js
+++ javascript/lecture6/script.js
@@ -1,27 +1,53 @@
-let div = document.querySelector("div");
-let btn1 = document.querySelector("#btn1");
+// let div = document.querySelector("div");
+// let btn1 = document.querySelector("#btn1");
 
-// 1. Get Attributes
-console.log(div.getAttribute("id"));
-console.log(div.getAttribute("name"));
+// // 1. Get Attributes
+// console.log(div.getAttribute("id"));
+// console.log(div.getAttribute("name"));
 
-// 2. Button Click Event
-btn1.onclick = (evt) => {
-    console.log("Button 1 was clicked");
-    let a = 25;
-    a++;
-    console.log(a); // 26
+// // 2. Button Click Event
+// btn1.onclick = (evt) => {
+//     console.log("Button 1 was clicked");
+//     let a = 25;
+//     a++;
+//     console.log(a); // 26
     
-    // Fixed capitalization for X and Y
-    console.log(evt.type);
-    console.log(evt.target);
-    console.log(evt.clientX, evt.clientY); 
-};
+//     // Fixed capitalization for X and Y
+//     console.log(evt.type);
+//     console.log(evt.target);
+//     console.log(evt.clientX, evt.clientY); 
+// };
 
-// 3. Mouseover Event (Removed the extra "let")
-div.onmouseover = (evt) => {
-    console.log("You are inside div");
-    console.log(evt.type);
-    console.log(evt.target);
-    console.log(evt.clientX, evt.clientY);
-};
+// // 3. Mouseover Event (Removed the extra "let")
+// div.onmouseover = (evt) => {
+//     console.log("You are inside div");
+//     console.log(evt.type);
+//     console.log(evt.target);
+//     console.log(evt.clientX, evt.clientY);
+// };
+// let modeBtn=document.querySelector("#mode");
+// let currMode="light";
+// modeBtn.addEventListener("click",()=>{
+//     if(currMode==="light"){
+//         currMode="dark";
+//         document.querySelector("body").style.backgroundColor ="black";
+
+//     }else{
+//         currMode="light";
+//         document.querySelector("body").style.backgroundColor ="white";
+//     }
+//     console.log(currMode);
+// });
+let modeBtn=document.querySelector("#mode");
+ let currMode="light";
+ modeBtn.addEventListener("click",()=>{
+     if(currMode==="light"){
+         currMode="dark";
+         document.querySelector("body").style.backgroundColor ="black";
+
+     }else{
+         currMode="light";
+         document.querySelector("body").style.backgroundColor ="white";
+     }
+     console.log(currMode);
+ });
