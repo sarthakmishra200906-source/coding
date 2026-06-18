c30f4e7c8b85048fc84fbb0d6e2d06222ad4da83
sarthakmishra200906 <sarthakmishra200906@gmail.com>
2026-06-18 12:25:24 +0530

    finished project 2 and learned basic oops

---
+++ python/oops/01_problem1.py
@@ -0,0 +1,12 @@
+class programer:
+    company ="microsoft"
+    def __init__(self,name,sallary,pin):
+        self.name = name
+        self.sallary = sallary  
+        self.pin = pin
+
+
+p= programer("sarthak",1200000,110011)
+print(p.name,p.sallary,p.pin,p.company)
+r= programer("ram",1000000,110022)
+print(r.name,r.sallary,r.pin,r.company)
